196eb0f5c803fc03f9d76ab5b6ab2bb372f4b74b
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2019-09-06 23:22:44 +1000

    fix addative overlay content / on scroll animation on overlay / added way to handle svg icons on links

--- src/modules/lsd/overlay/overlay.js
+++ src/modules/lsd/overlay/overlay.js
@@ -5,22 +5,55 @@ export default class Overlay extends LightningElement {
     @api overlayClassName;
     @api selectedProjectId;
     @track selectedProjectData;
+    @track showScrolledHeaderClass = 'lsd-overlay_wrapper_header';
+    @track showScrolledImageClass = 'lsd-overlay_wrapper_image';
 
     renderedCallback() {
-        console.log('Overlay renderedCallback() projects', projects);
-        console.log('Overlay this.selectedProjectId ', this.selectedProjectId);
-
         let newProject = projects.filter(
             item => item.id === this.selectedProjectId
         );
         this.selectedProjectData = newProject[0] ? newProject[0] : null;
-        console.log('Overlay selectedProjectData', this.selectedProjectData);
+
+        // lsd-overlay_wrapper_image_src
+
+        let projectBg = this.template.querySelector('[data-header-image]');
+
+        let projectLogo = this.template.querySelector('[data-logo]');
+        projectBg && projectLogo
+            ? (projectBg.style.backgroundColor = projectLogo.dataset.brandColor)
+            : null;
+        projectLogo
+            ? (projectLogo.style.backgroundImage = `url("${projectLogo.dataset.logo}")`)
+            : null;
+
+        let wrapperContent = this.template.querySelector(
+            '.lsd-overlay_wrapper_content'
+        );
+        wrapperContent
+            ? wrapperContent.addEventListener(
+                  'scroll',
+                  this.handleScroll.bind(this)
+              )
+            : null;
+    }
+
+    @api
+    handleScroll(e) {
+        e.target.scrollTop <= 50
+            ? (this.showScrolledHeaderClass = 'lsd-overlay_wrapper_header') &&
+              (this.showScrolledImageClass = 'lsd-overlay_wrapper_image')
+            : (this.showScrolledHeaderClass =
+                  'lsd-overlay_wrapper_header active') &&
+              (this.showScrolledImageClass =
+                  'lsd-overlay_wrapper_image active');
     }
 
     toggleOverlayParent(e) {
         e.explicitOriginalTarget.classList.contains('visible') ||
         e.explicitOriginalTarget.classList.contains('visible')
-            ? this.dispatchEvent(new CustomEvent('overlaytoggle'))
+            ? this.dispatchEvent(new CustomEvent('overlaytoggle')) &&
+              (this.showScrolledImageClass = 'lsd-overlay_wrapper_image') &&
+              (this.showScrolledHeaderClass = 'lsd-overlay_wrapper_header')
             : null;
     }
 }
